65b35549d8fd6f30de6c9c0c9807510ccb913672
5tormyzz <susi.bussiness@gmail.com>
2025-07-02 08:46:32 +0300

    Implemented logic for checking allowed equipment and items in mobspawning and tidied up the project

--- Wanted/src/main/java/me/stormyzz/wanted/Wanted.java
+++ Wanted/src/main/java/me/stormyzz/wanted/Wanted.java
@@ -12,35 +12,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Wanted extends JavaPlugin {
 
-    private static Wanted instance;
     private MobSpawnerManager mobSpawnerManager;
     private CustomScoreboard customScoreboard;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        instance = this;
-        saveDefaultConfig();
-	System.out.println("Server Start");
-        mobSpawnerManager = new MobSpawnerManager(this);
         customScoreboard = new CustomScoreboard(this);
+        mobSpawnerManager = new MobSpawnerManager(this);
         mobSpawnerManager.loadConfig();
+        saveDefaultConfig();
 
         getServer().getPluginManager().registerEvents(new MobSpawnListener(mobSpawnerManager, this), this);
         getServer().getPluginManager().registerEvents(customScoreboard, this);
-        getCommand("wantedreload").setExecutor(new WantedCommand(this));
         getServer().getPluginManager().registerEvents(new MobBurnPreventionListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerKillListener(this), this);
+        getServer().getPluginManager().registerEvents(new CustomScoreboard(this), this);
+        Bukkit.getPluginManager().registerEvents(new DeathBalanceListener(this), this);
+        getCommand("wantedreload").setExecutor(new WantedCommand(this));
 
 
-        // Schedule the stats saving task every 15 minutes
         Bukkit.getScheduler().runTaskTimerAsynchronously(
-                this, // Your plugin instance
-                () -> playerStatsManager.saveStats(), // Task to save stats
-                0L, // Initial delay (0 means start immediately)
-                20L * 60 * 15 // Repeat interval (2 minutes in ticks)
+                this,
+                () -> playerStatsManager.saveStats(),
+                0L,
+                20L * 60 * 15
         );
 
-        // Register shutdown hook to save stats on server shutdown
+        //Statistics saver
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
                 playerStatsManager.saveStats();
@@ -51,13 +49,7 @@ public final class Wanted extends JavaPlugin {
             }
         }));
 
-        // Register the player kill and join listeners
-        getServer().getPluginManager().registerEvents(new PlayerKillListener(this), this);
-        getServer().getPluginManager().registerEvents(new CustomScoreboard(this), this);
-        Bukkit.getPluginManager().registerEvents(new DeathBalanceListener(this), this);
-
-
-        // Make sure Vault is set up before loading stats
+        //Vault getter
         Bukkit.getScheduler().runTask(this, () -> {
             if (!VaultEconomyManager.setupEconomy()) {
                 getLogger().warning("Vault not found! Economy features may not work.");
@@ -66,10 +58,10 @@ public final class Wanted extends JavaPlugin {
             }
             getLogger().info("Economy setup successfully.");
 
-            // Now load player stats, since Vault is confirmed to be available
             playerStatsManager.loadStats();
         });
 
+        //Worldguard getter
         WorldGuardPlugin wg = getWorldGuard();
         if (wg == null){
             getLogger().severe("WorldGuard plugin not found! mob spawning will not work");
@@ -86,6 +78,7 @@ public final class Wanted extends JavaPlugin {
     public MobSpawnerManager getMobSpawnerManager(){
         return mobSpawnerManager;
     }
+    public CustomScoreboard getCustomScoreboard() { return customScoreboard; }
 
     @Override
     public void onDisable() {
@@ -94,11 +87,4 @@ public final class Wanted extends JavaPlugin {
         Bukkit.getLogger().info("Player stats saved successfully during shutdown.");
     }
 
-    public static Wanted getInstance() {
-        return instance;
-    }
-
-    public CustomScoreboard getCustomScoreboard() {
-        return customScoreboard;
-    }
 }
